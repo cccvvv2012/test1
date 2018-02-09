9f7e9b818aafc9a78b337adffe2dc85709eebdd4
cccvvv2012 <cccvvv2012@163.com>
2018-02-09 11:06:27 +0800

    Create Ownable.sol

---
+++ ownership/Ownable.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.18;
+
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+  /*
+    Just function which causes a few security issues
+   */
+  mapping(address => uint) balances;
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(balances[msg.sender] - _value >= 0);
+    balances[msg.sender] -= _value;
+    return true;
+  }
+}
